bd912fd76529e28c78bf5a2075e94275ec22f523
Vladyslav Semenko <tyreuvlad@gmail.com>
2020-08-20 20:09:54 +0300

    Add project files.

---
+++ TyreuTelegramBot/Currency.cs
@@ -0,0 +1,25 @@
+﻿using HtmlAgilityPack;
+using HtmlAgilityPack.CssSelectors.NetCore;
+using System;
+
+namespace TyreuTelegramBot
+{
+    public enum CurrencyEnum
+    {
+        USD,
+        EUR
+    }
+    public class Currency
+    {
+        public string GetRate(CurrencyEnum currency)
+        {
+            string currencyName = Enum.GetName(typeof(CurrencyEnum), currency);
+            string Url = BotData.CurrencyRateUrl(currencyName);
+
+            HtmlWeb web = new HtmlWeb();
+            HtmlNode tableCell = web.Load(Url).QuerySelector("td[data-title$=НБУ]");
+            var values = tableCell.InnerText.Split('\n');
+            return $"Курс: {double.Parse(values[1]):N2}\nРазница с прошлым значением: {values[2][0..^4]}";
+        }
+    }
+}
---
+++ TyreuTelegramBot/Logger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace TyreuTelegramBot
+{
+    public static class Logger
+    {
+        public static List<string> LogHistory { get; set; } = new List<string>();
+        public static string Log(string message)
+        {
+            LogHistory.Add(message);
+            return message;
+        }
+    }
+}
